f8b651f938ee0f8236a2fe460415bba0dabc930f
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-08-05 19:14:48 +0700

    4.Passing-Params-to-route

---
+++ src/screens/listSoc.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import { Text, Box, FlatList, Pressable } from "native-base";
+
+// Add Props in Hello({navigation})
+export default function Hello({ navigation }) {
+  // Set Dummy Data with Array
+
+  // Make Function handle press to get value per list
+
+  return (
+    <Box
+      safeArea
+      bg="primary.400"
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      p={10}
+    >
+      {/* Render Array With Flatlist */}
+    </Box>
+  );
+}
