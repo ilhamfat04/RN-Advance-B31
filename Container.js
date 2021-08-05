a148d2bcf13073e97b4e1340a6e180c36f21238c
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-08-05 18:17:19 +0700

    3.Bottom-Tabs-Navigation

--- Container.js
+++ Container.js
@@ -1,10 +1,17 @@
 import * as React from "react";
 
 //Import Navigation Container
+import { NavigationContainer } from "@react-navigation/native";
 
 // Import Stack Navigation
+import { createStackNavigator } from "@react-navigation/stack";
+
+//Import Bottom Tab Navigation
+
+//Import Icon
 
 // Import Theme Native Base
+import { useTheme } from "native-base";
 
 // Import Screen
 import FormNativeBase from "./src/screens/formNativeBase";
@@ -12,8 +19,41 @@ import Hello from "./src/screens/hello";
 import IncDec from "./src/screens/incDec";
 
 // Create Stack Navigation
+const Stack = createStackNavigator();
+
+//Create Bottom Tab Navigation
+
+// Create Component Bottom Tab Navigation
 
 export default function Container() {
   // Init Theme
-  return; // Refactor Code Navigation
+  const theme = useTheme();
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerMode: "screen",
+          headerTintColor: "white",
+          headerStyle: { backgroundColor: theme.colors.primary["300"] },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={Hello}
+          options={{
+            title: "Hello Screen",
+          }}
+        />
+        <Stack.Screen
+          name="IncDec"
+          component={IncDec}
+          options={{
+            title: "Increment Decrement",
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- src/screens/hello.js
+++ src/screens/hello.js
@@ -1,15 +1,34 @@
 import * as React from "react";
-import { Text, Box } from "native-base";
+import { Text, Box, Pressable } from "native-base";
 
 // Add Props in Hello({navigation})
-export default function Hello() {
+export default function Hello({ navigation }) {
   return (
-    <Box bg="primary.400" flex={1} alignItems="center" justifyContent="center">
+    <Box
+      bg="primary.400"
+      flex={1}
+      alignItems="center"
+      justifyContent="center"
+      p={10}
+    >
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         Life's too short
       </Text>
 
-      {/* Create Button */}
+      <Pressable
+        onPress={() => navigation.navigate("IncDec")}
+        style={{
+          backgroundColor: "#487eb0",
+          height: 40,
+          width: "100%",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 10,
+          margin: 20,
+        }}
+      >
+        <Text color={{ color: "white" }}>Screen Increment and Decrement</Text>
+      </Pressable>
     </Box>
   );
 }
