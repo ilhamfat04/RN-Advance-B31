4e63eef020936e5c36824e2e1fac28fda6f44a39
= <=>
2022-03-30 15:01:21 +0700

    update

--- src/screens/detailSoc.js
+++ src/screens/detailSoc.js
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Text, Box } from "native-base";
 
 // Declare route
-export default function DetailSoc({}) {
+export default function DetailSoc({ route }) {
+
+  console.log(route)
   return (
     <Box
       safeArea
@@ -14,6 +16,11 @@ export default function DetailSoc({}) {
     >
       <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
         {/* Implement Params to get Data on previous screen  */}
+        {route.params.value}
+      </Text>
+      <Text fontFamily="body" fontWeight={400} fontStyle="italic" fontSize={30}>
+        {/* Implement Params to get Data on previous screen  */}
+        {route.params.data}
       </Text>
     </Box>
   );
--- src/screens/listSoc.js
+++ src/screens/listSoc.js
@@ -4,8 +4,13 @@ import { Text, Box, FlatList, Pressable } from "native-base";
 // Add Props in Hello({navigation})
 export default function Hello({ navigation }) {
   // Set Dummy Data with Array
+  const socialMedia = ["Tiktok", "Instagram", "Twitter", "Facebook"]
 
   // Make Function handle press to get value per list
+  const handlePress = (value) => {
+    const data = value + "data"
+    navigation.navigate("Detail Social", { value, data })
+  }
 
   return (
     <Box
@@ -16,7 +21,19 @@ export default function Hello({ navigation }) {
       justifyContent="center"
       p={10}
     >
-      {/* Render Array With Flatlist */}
+      <FlatList
+        data={socialMedia}
+        renderItem={({ item }) => (
+          <Pressable onPress={() => handlePress(item)} >
+            <Text
+              fontFamily="body"
+              fontWeight={400}
+              fontSize={30}
+              marginBottom={5}
+            >{item}</Text>
+          </Pressable>
+        )}
+      />
     </Box>
   );
 }
