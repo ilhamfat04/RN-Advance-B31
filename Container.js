d9ada74192e1d59100b11fd894b714633c906e93
= <=>
2022-03-30 14:35:43 +0700

    update

--- Container.js
+++ Container.js
@@ -7,8 +7,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 //Import Bottom Tab Navigation
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 
 //Import Icon
+import { Ionicons } from "@expo/vector-icons"
 
 // Import Theme Native Base
 import { useTheme } from "native-base";
@@ -22,8 +24,42 @@ import IncDec from "./src/screens/incDec";
 const Stack = createStackNavigator();
 
 //Create Bottom Tab Navigation
+const Tab = createBottomTabNavigator()
 
 // Create Component Bottom Tab Navigation
+function MyTab() {
+  const theme = useTheme()
+
+  return (
+    <Tab.Navigator
+      initialRouteName="Hello"
+      screenOptions={({ route }) => ({
+        headerMode: "screen",
+        headerTintColor: "white",
+        headerStyle: { backgroundColor: theme.colors.primary["300"] },
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName
+
+          if (route.name === "Hello") {
+            iconName = focused ? "ios-home" : "ios-home-outline"
+          } else if (route.name == "Form") {
+            iconName = focused ? "information-circle" : "information-circle-outline"
+          } else if (route.name == "IncDec") {
+            iconName = focused ? "information-circle" : "information-circle-outline"
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+        tabBarActiveTintColor: theme.colors.primary["800"],
+        tabBarInactiveTintColor: "red"
+      })}
+    >
+      <Tab.Screen name="Hello" component={Hello} options={{ headerShown: false }} />
+      <Tab.Screen name="Form" component={FormNativeBase} />
+      <Tab.Screen name="IncDec" component={IncDec} />
+    </Tab.Navigator>
+  )
+}
 
 export default function Container() {
   // Init Theme
@@ -40,10 +76,10 @@ export default function Container() {
         }}
       >
         <Stack.Screen
-          name="Home"
-          component={Hello}
+          name="Main"
+          component={MyTab}
           options={{
-            title: "Hello Screen",
+            headerShown: false
           }}
         />
         <Stack.Screen
